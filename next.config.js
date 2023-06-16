b41592bb21b2c3713b9405285f3e82b1804a8eb3
Angel Lopez <a.o.lopez.cruz@gmail.com>
2023-06-16 14:38:30 -0600

    🙈 adding variables environment for my API Keys of Back4App

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    PARSE_APPLICATION_ID: process.env.PARSE_APPLICATION_ID,
+    PARSE_HOST_URL: process.env.PARSE_HOST_URL,
+    PARSE_JAVASCRIPT_KEY: process.env.PARSE_JAVASCRIPT_KEY,
+  },
+}
 
 module.exports = nextConfig
